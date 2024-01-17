12ce7570f7fb13e5b8ce21996c33d96bc357a933
mr1devo <128217343+mr1devo@users.noreply.github.com>
2024-01-16 18:27:52 -0930

    Add files via upload

---
+++ model/signup.js
@@ -0,0 +1,19 @@
+
+const mongoose = require('mongoose');
+
+const signupSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model('User', signupSchema);
+
+module.exports = User;
